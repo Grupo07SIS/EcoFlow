974a3db716e982913b3da63bdc288fff5c19a2a1
EduardoMNeris <eduardo.neris@sptech.school>
2024-06-13 19:09:49 -0300

    Update - Atualições na navbar

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.marginLeft = "0";
+  }
+  function closeNav() {
+    document.getElementById("mySidenav").style.marginLeft = "-230px";
+  }
