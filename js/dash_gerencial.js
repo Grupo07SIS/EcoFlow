584440593f357c1a94d970be617a594ee7e8d80f
Paola Gomes <125298327+paola-gomes@users.noreply.github.com>
2024-06-14 00:27:53 -0300

    ADD - estilização feita em 95%

--- js/dash_gerencial.js
+++ js/dash_gerencial.js
@@ -1,21 +1,33 @@
-function gerarGrafico(){
-const ctx = document.getElementById('myChart');
+// colocar @CrossOrigin no back antes do @GetMapping
+async function gerarGrafico() {
+  // const resposta = await fetch("http://localhost:8080/dashboar/grafico-inscritos");
+  // if (!resposta.ok){
+  //   throw new Error (`HTTP error! Status: ${resposta.status}`)
+  // }
+  // const respostaDados = await resposta.json();
+  // console.log(respostaDados)
+
+  const ctx = document.getElementById('myChart');
 
   new Chart(ctx, {
     type: 'pie',
     data: {
-      labels: ['Green', 'Orange'],
+      labels: ['Vagas Abertas', 'Vagas Preenchidas'],
       datasets: [{
-        label: '# of Votes',
+        label: ['Vagas Abertas', 'Vagas Preenchidas'],
         data: [12, 5],
-        borderWidth: 1
+        borderWidth: 1,
+        backgroundColor: [
+          'rgba(22, 57, 21, 1)',
+          'rgba(199, 83, 12, 1)'
+        ]
       }]
     },
     options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        display: false
       }
     }
   })
@@ -23,3 +35,22 @@ const ctx = document.getElementById('myChart');
   document.getElementById("valor_gasto").innerHTML = "1000,00"
 }
 
+// Possivel cod para barra lucro de evento
+
+// resposta.json().then(function (resposta) {
+//   console.log("Dados recebidos: ", JSON.stringify(resposta));
+//   var porcentagem = document.getElementById("porcentage");
+//   porcentagem.innerHTML = ${resposta[0].Uso_DIsco} %;
+//   var bar = document.querySelector(".lucro_alcancado");
+//       var porcentagemUso = parseFloat(resposta[0].Uso_DIsco);
+//       // Check if the value is more than 60%
+//       if (porcentagemUso > 60) {
+//           bar.style.backgroundColor = "#FFE500";
+//       }
+//       // Check if the value is more than 80%
+//       if (porcentagemUso > 80) {
+//           bar.style.backgroundColor = "#ff7800";
+//       }
+//       // Set the width based on the value
+//       bar.style.width = ${porcentagemUso}%;
+// });
