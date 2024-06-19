b5efe498ff68fd5e3abea46c16cb0b34ffd5aab4
Paola Gomes <125298327+paola-gomes@users.noreply.github.com>
2024-06-18 23:38:35 -0300

    ADD - KPIs funcionais puxando do backend

--- js/dash_gerencial.js
+++ js/dash_gerencial.js
@@ -1,12 +1,4 @@
-// colocar @CrossOrigin no back antes do @GetMapping
 async function gerarGrafico() {
-  // const resposta = await fetch("http://localhost:8080/dashboar/grafico-inscritos");
-  // if (!resposta.ok){
-  //   throw new Error (`HTTP error! Status: ${resposta.status}`)
-  // }
-  // const respostaDados = await resposta.json();
-  // console.log(respostaDados)
-
   const resposta = await fetch ("http://localhost:8080/dashboard/grafico-inscritos?startDate=2000-01-01&endDate=2024-06-20")
   const respostaDados = await resposta.json()
   console.log("Resposta:", respostaDados)
@@ -39,25 +31,35 @@ async function gerarGrafico() {
     }
   })
 
-  document.getElementById("valor_gasto").innerHTML = "000.00"
+  // KPI valor gasto
+  const resposta_gasto = await fetch ("http://localhost:8080/dashboard/kpi-gasto-total?startDate=2000-01-01&endDate=2024-06-20")
+  const respostaDados_gasto = await resposta_gasto.json()
+  console.log("Resposta valor gasto:", respostaDados_gasto, "Deus é TOP!")
+  document.getElementById("valor_gasto").innerHTML = respostaDados_gasto
+
+  // KPI orçamento
+  const resposta_orcamento = await fetch ("http://localhost:8080/dashboard/kpi-orcamento?startDate=2000-01-01&endDate=2024-06-20")
+  const respostaDados_orcamento = await resposta_orcamento.json()
+  console.log("Resposta orçamento:", respostaDados_orcamento, "Deus > nome que não deve ser dito")
+  document.getElementById("orcamento").innerHTML = respostaDados_orcamento
+
+  // KPI valor lucro
+  const resposta_lucro = await fetch ("http://localhost:8080/dashboard/kpi-valor-lucro?startDate=2000-01-01&endDate=2024-06-20")
+  const respostaDados_lucro = await resposta_lucro.json()
+  console.log("Resposta orçamento:", respostaDados_lucro, "Jesus é luz")
+  document.getElementById("valor_lucro").innerHTML = respostaDados_lucro
+
+  //  KPI número de colaboradores incritos 
+  document.getElementById("colaboradores").innerHTML = inscritos
+
+  // KPI colaboradores reprovados
+  const resposta_reprovados = await fetch ("http://localhost:8080/dashboard/kpi-reprovados?startDate=2000-01-01&endDate=2024-06-20")
+  const respostaDados_reprovados = await resposta_reprovados.json()
+  console.log("Resposta orçamento:", respostaDados_reprovados, "Deus tudinho, di@bo nadinha")
+  document.getElementById("colab_reprovados").innerHTML = respostaDados_reprovados
+
+  // KPI média de avaliações
+  document.getElementById("media_avaliacoes").innerHTML = 8.76
+ 
+
 }
-
-// Possivel cod para barra lucro de evento
-
-// resposta.json().then(function (resposta) {
-//   console.log("Dados recebidos: ", JSON.stringify(resposta));
-//   var porcentagem = document.getElementById("porcentage");
-//   porcentagem.innerHTML = ${resposta[0].Uso_DIsco} %;
-//   var bar = document.querySelector(".lucro_alcancado");
-//       var porcentagemUso = parseFloat(resposta[0].Uso_DIsco);
-//       // Check if the value is more than 60%
-//       if (porcentagemUso > 60) {
-//           bar.style.backgroundColor = "#FFE500";
-//       }
-//       // Check if the value is more than 80%
-//       if (porcentagemUso > 80) {
-//           bar.style.backgroundColor = "#ff7800";
-//       }
-//       // Set the width based on the value
-//       bar.style.width = ${porcentagemUso}%;
-// });
