4902ef9f196a1bdfe25d3080cdb995db54c314d8
Kaori2 <michelly.katayama@sptech.school>
2024-07-27 17:25:52 -0300

    ADD - Nova tela de participantes em espera parte de front feito.

---
+++ js/filter.js
@@ -0,0 +1,7 @@
+
+function openFilter() {
+    document.getElementById("filterMain").style.marginTop = "56px";
+  }
+  function closeFilter() {
+    document.getElementById("filterMain").style.marginTop = "800px";
+  }
